fcc1e54f00955e6907670ff0a6e86946cbda0a96
RobertoUa <roberto.te.ua@gmail.com>
2013-03-29 12:39:40 +0200

    add foreman, another try with travis

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,6 @@
+$stdout.sync = true
 RaisShop::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
